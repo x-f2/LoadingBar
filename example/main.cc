7e528779b8fad8fd4a11f71543ca60e1abee9544
x-f2 <113227770+x-f2@users.noreply.github.com>
2022-10-10 19:04:20 -0400

    Update main.cc

--- example/main.cc
+++ example/main.cc
@@ -8,6 +8,9 @@ int main() {
 	int packagetotal = 100;
 	LoadingBar bar;
 	bar.addTask(packages, packagetotal);
+	bar.setPrefix("Progress:");
+	bar.setSuffix("Complete.");
+	
 	std::thread s = bar.start();
 	while (packages < packagetotal) {
 		packages += rand() % 10 + 1;
